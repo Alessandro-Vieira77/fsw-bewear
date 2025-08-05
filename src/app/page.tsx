3ceabb3fbccba37cdfd2552c87be6ce4630a6ad1
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-08-04 22:34:37 -0300

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1 className="m-5 font-bold text-blue-500">FSW BEWEAR</h1>
       <Button>FSW BEWEAR</Button>
     </div>
   );
